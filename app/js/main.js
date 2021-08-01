9b7f3efbe6a5a22c4d7f44ca8bd01c20c64e5a50
AlexandrShnipov <sshnipov@gmail.com>
2021-08-02 00:40:37 +0300

    added media, and burger-btn

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
-$(function (){
-   
-})
+let burgerBtn = document.querySelector('.burger-btn');
+
+function burgerBtnActive (){
+
+burgerBtn.addEventListener('click' , toggleBurgerBtn);
+}
+
+burgerBtnActive()
+
+function toggleBurgerBtn() {
+  console.log('close');
+  burgerBtn.classList.toggle('active');
+}
